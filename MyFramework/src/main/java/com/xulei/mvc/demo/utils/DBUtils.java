8db5120553f35143e91049c01a5a2830c3715bee
xulei <ashura1110@163.com>
2019-01-05 18:39:59 +0800

    1：修改数据库连接为ThreadLocal

--- MyFramework/src/main/java/com/xulei/mvc/demo/utils/DBUtils.java
+++ MyFramework/src/main/java/com/xulei/mvc/demo/utils/DBUtils.java
@@ -13,59 +13,51 @@ public class DBUtils {
     private static final String password="root";
 
 
-    private static Connection conn=null;
+//    private static Connection conn=null;
+
+    //如果在多线程情况下，如果所有线程都公用一个连接。会可能出现线程1关闭线程2的连接。所以需要每一个线程都拥有自己的连接
+    //使用ThreadLocal来实现
+
+    private static ThreadLocal<Connection> connContainers=new ThreadLocal<Connection>();
+
+
 
     //1:获取连接
-
     public static Connection getConnection(){
 
         System.out.println("-----------------开始获取数据库连接了--------------------------");
+
+        Connection conn = connContainers.get();
         try {
-            //1:注册驱动
-            Class.forName(driver);
-            //2:获得数据库链接
-            conn = DriverManager.getConnection(url,userName,password);
+            if(conn==null){
+                //1:注册驱动
+                Class.forName(driver);
+                //2:获得数据库链接
+                conn = DriverManager.getConnection(url,userName,password);
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            connContainers.set(conn);
         }
         return conn;
 
     }
 
 
-//    public static  Object executeQuerySQl(String sql) throws SQLException {
-//
-//        Connection connection = getConnection();
-//
-//        Statement statement = connection.createStatement();
-//
-//        ResultSet resultSet = statement.executeQuery(sql);
-//
-//        while (resultSet.next()){
-//
-//            String data = resultSet.getString(1);
-//            return data;
-//        }
-//
-//        return null;
-//    }
-
     //关闭连接
     public static void closeConnection() {
-
+        Connection conn = connContainers.get();
         try {
             if(conn!=null){
                 conn.close();
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            connContainers.remove();
         }
     }
-
-
-
-
-
 }
